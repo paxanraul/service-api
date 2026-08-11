6e31b9a581be7ac67bef28032c8202d410c9821b
paxanraul <raul20081033@gmail.com>
2026-08-11 16:42:38 +0300

    add: delete product

---
+++ app/delete_product.py
@@ -0,0 +1,14 @@
+from sqlalchemy import select, delete
+
+from app.db import SessionLocal
+from app.models import Product
+
+
+with SessionLocal() as session:
+	statement = select(Product).where(Product.id == 1)
+	product = session.scalar(statement)
+
+	if product is not None:
+		session.delete(product)
+		session.commit()
+		print("Successfully deleted!")
