9439ba06bbf6d7bd74b5185e6d46280ea2cea084
paxanraul <raul20081033@gmail.com>
2026-08-14 17:31:40 +0300

    feat: add created_at

--- app/models/models.py
+++ app/models/models.py
@@ -1,7 +1,8 @@
 from app.db import Base
 
 from decimal import Decimal
-from sqlalchemy import String
+from datetime import datetime
+from sqlalchemy import String, func
 from sqlalchemy.orm import Mapped, mapped_column
 
 class Product(Base):
@@ -11,3 +12,4 @@ class Product(Base):
 	name: Mapped[str] = mapped_column(String(20))
 	price: Mapped[Decimal] = mapped_column(nullable=False)
 	description: Mapped[str | None] = mapped_column(String(150))
+	created_at: Mapped[datetime] = mapped_column(default=func.now())
