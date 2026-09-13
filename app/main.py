2c18a0fe2c7544c49e997164e099af0911a11e0d
paxanraul <raul20081033@gmail.com>
2026-09-13 18:41:14 +0300

    include health router in main

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 
 from app.api.products import router as products_router
+from app.api.health import router as health_router
 
 
 app = FastAPI(title="Service API")
 
 app.include_router(products_router)
+app.include_router(health_router)
