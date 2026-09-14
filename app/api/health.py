ef11c32082da6d193cf627d0a74ca7c7cfab5323
paxanraul <raul20081033@gmail.com>
2026-09-14 21:09:32 +0300

    feat: add try/except in health_check

--- app/api/health.py
+++ app/api/health.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy import text
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import SQLAlchemyError
 
 from app.dependencies import get_db
 
@@ -8,5 +9,11 @@ router = APIRouter(tags=["health"])
 
 @router.get("/health")
 def health_check(db: Session = Depends(get_db)):
-    db.execute(text("SELECT 1"))
+    try:
+        db.execute(text("SELECT 1"))
+    except SQLAlchemyError:
+        raise HTTPException(
+            status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+            detail="База не работает"
+        )
     return {"status": "ok"}
