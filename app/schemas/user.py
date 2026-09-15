ed56b9689e2d51f97a850a71423a84788351b748
paxanraul <raul20081033@gmail.com>
2026-09-15 13:58:46 +0300

    feat: add user schema

---
+++ app/schemas/user.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from pydantic import BaseModel, EmailStr, ConfigDict
+
+class UserRegister(BaseModel):
+    email: EmailStr
+    password: str
+
+class UserResponse(BaseModel):
+    id: int
+    email: EmailStr
+    created_at: datetime
+
+    model_config = ConfigDict(from_attributes=True)
