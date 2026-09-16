a0cffedc91559809d6125e42a0e4b9fcd77fd06b
paxanraul <raul20081033@gmail.com>
2026-09-16 11:28:49 +0300

    add: user_repo

---
+++ app/repositories/user_repo.py
@@ -0,0 +1,22 @@
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from app.models.user import User
+
+
+def get_user_by_email_repo(user_email: str, db: Session) -> User | None:
+    statement = select(User).where(User.email == user_email)
+    user = db.scalar(statement)
+
+    return user
+
+
+def create_user_repo(user: User, db: Session) -> User:
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+
+    return user
+
+
+
