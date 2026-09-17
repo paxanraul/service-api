df9050bba595083d89ffdc23df362b4c55c877f9
paxanraul <raul20081033@gmail.com>
2026-09-17 13:13:43 +0300

    feat: icluding router in main

--- app/main.py
+++ app/main.py
@@ -2,9 +2,11 @@ from fastapi import FastAPI
 
 from app.api.products import router as products_router
 from app.api.health import router as health_router
+from app.api.users import router as users_router
 
 
 app = FastAPI(title="Service API")
 
+app.include_router(users_router)
 app.include_router(products_router)
 app.include_router(health_router)
